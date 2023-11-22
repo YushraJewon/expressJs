5e13e91cb293355fdd1d982fca4b3fe829c4f119
HashirRajah <hashir.rajah@spoonconsulting.com>
2023-11-22 22:22:02 +0400

    Pagination

--- server.js
+++ server.js
@@ -10,22 +10,71 @@ app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap-icons/font'));
 
 app.get("/", async (req, res) => {
-  let movies = []
+  let index = 1;
+  let start = req.query.page ?? 1;
+  start = parseInt(start);
+  let activePage = start;
+  let max = 10;
+  let movies = [];
   let websiteName = "ExpressMovies";
+  let numberOfRecords = getNumberOfRows("./movies.csv");
+  let paginations = buildPagination(numberOfRecords, max);
+
   
-   fs.createReadStream("./movies.csv")
-    .pipe(parse({ delimiter: ",", from_line: 2 }))
-    .on("data", function (row) {
+  let readStream = fs.createReadStream("./movies.csv");
+  readStream
+  .pipe(parse({ delimiter: ",", from_line: 2 }))
+  .on("data", function (row) {
+    
+    if(index === (start + max)){
+      readStream.unpipe(parse({ delimiter: ",", from_line: 2 }));
+      return false;
+    }
+
+    if(index >= start){
       let movie = {
         'name': row[0],
         'genre': row[1],
         'year': row[7]
       };
+  
       movies.push(movie);
-    })
-    .on('end', () => {
-      res.render("home", { name: websiteName, movies: movies })
-    });
+    }
+
+    index++;
+
+  })
+  .on('end', () => {
+    res.render("home", { name: websiteName, movies: movies, paginations: paginations, 'active': activePage });
+  });
 });
+
+function getNumberOfRows(filePath) {
+  const csvContent = fs.readFileSync(filePath, 'utf8');
+
+  const rows = csvContent.split('\n').length;
+
+  return rows;
+}
+
+function buildPagination(numberOfRecords, maxRecords){
+  let paginations = [];
+  let page = 1;
+
+  const numberOfPages = Math.ceil(numberOfRecords / maxRecords);
+  
+  for(let i = 1;i <= numberOfPages; i++){
+    let pagination = {
+      key: i,
+      page: page
+    };
+
+    paginations.push(pagination);
+
+    page += maxRecords;
+  }
+
+  return paginations;
+}
 
 app.listen(3000)
