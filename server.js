b7b35dcd2abb1ff1949daea663845ec871c61f21
HashirRajah <hashir.rajah@spoonconsulting.com>
2023-11-23 14:06:11 +0400

    Database

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-const express = require("express")
-const app = express()
-const fs = require("fs")
-const { parse } = require("csv-parse") 
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const { parse } = require("csv-parse"); 
+const cache = require("memory-cache");
+const mysql = require("mysql");
 
 app.set("view engine", "pug");
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap-icons/font'));
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'node_project'
+});
+
+connection.connect();
+
 
 app.get("/", async (req, res) => {
   let index = 1;
@@ -20,33 +32,19 @@ app.get("/", async (req, res) => {
   let numberOfRecords = getNumberOfRows("./movies.csv");
   let paginations = buildPagination(numberOfRecords, max);
 
-  
-  let readStream = fs.createReadStream("./movies.csv");
-  readStream
-  .pipe(parse({ delimiter: ",", from_line: 2 }))
-  .on("data", function (row) {
-    
-    if(index === (start + max)){
-      readStream.unpipe(parse({ delimiter: ",", from_line: 2 }));
-      return false;
-    }
+  let cachedResults = getCachedData(start, (start + (max - 1)));
+  movies = cachedResults.hits;
 
-    if(index >= start){
-      let movie = {
-        'name': row[0],
-        'genre': row[1],
-        'year': row[7]
-      };
-  
-      movies.push(movie);
-    }
-
-    index++;
-
-  })
-  .on('end', () => {
+  if (cachedResults.misses.length === 0) {
     res.render("home", { name: websiteName, movies: movies, paginations: paginations, 'active': activePage });
-  });
+
+    return;
+  }
+
+  let additionalMovies = fetchDataFromDatabase(start, (start + (max - 1)));
+  movies.concat(additionalMovies);
+
+  res.render("home", { name: websiteName, movies: movies, paginations: paginations, 'active': activePage });
 });
 
 function getNumberOfRows(filePath) {
@@ -75,6 +73,84 @@ function buildPagination(numberOfRecords, maxRecords){
   }
 
   return paginations;
+}
+
+function getCachedData (startingKey, endingKey) {
+  let cacheMiss = [];
+  let cacheHits = [];
+
+  for (i = startingKey; i <= endingKey; i++) {
+    const cachedData = cache.get(i);
+
+    if (cachedData) {
+      cacheHits.push(cachedData);
+      console.log("Cache Hit.");
+
+      continue;
+    }
+
+    cacheMiss.push(i);
+    console.log("Cache Miss.");
+  }
+
+  return {
+    hits: cacheHits,
+    misses: cacheMiss
+  };
+}
+
+function fetchDataFromDatabase (startingIndex, endingIndex) {
+  let data = [];
+
+  for (i = startingIndex; i <= endingIndex; i++) {
+
+    connection.query(`SELECT name, genre, year FROM movies WHERE id = ?;`, [i], (err, rows, fields) => {
+      if (err) throw err
+
+      let movie = {
+        'name': rows[0].name,
+        'genre': rows[0].genre,
+        'year': rows[0].year
+      };
+
+      cache.put(i, movie);
+      data.push(movie);
+    });
+  }
+
+  console.log("Fetched from Database.", data);
+
+  return data;
+}
+
+function fetchDataFromCSV (index, start, max) {
+  let readStream = fs.createReadStream("./movies.csv");
+  readStream
+  .pipe(parse({ delimiter: ",", from_line: 2 }))
+  .on("data", function (row) {
+    
+    if (index === (start + max)) {
+      readStream.unpipe(parse({ delimiter: ",", from_line: 2 }));
+      return false;
+    }
+
+    if (index >= start) {
+      let movie = {
+        'name': row[0],
+        'genre': row[1],
+        'year': row[7]
+      };
+      
+      movies.push(movie);
+      cache.put(index, movie);
+    }
+
+    index++;
+
+  })
+  .on('end', () => {
+    res.render("home", { name: websiteName, movies: movies, paginations: paginations, 'active': activePage });
+  });
 }
 
 app.listen(3000)
